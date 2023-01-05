5e3135c0d1bb817678e9f52dae67e7832d1ab940
Bashar Pati <bjpati@kth.se>
2023-01-05 18:03:46 +0100

    Delete config

--- TW3/src/apiConfig.js
+++
@@ -1,5 +0,0 @@
-const BASE_URL="https://brfenergi.se/iprog/group/99/"; // the DH2642 proxy server
-const API_KEY="3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767";
-//TODO: export (not default) BASE_URL and API_KEY
-// then in other files you can write import {BASE_URL, API_KEY} from /src/apiConfig.js;
-export {BASE_URL, API_KEY}
